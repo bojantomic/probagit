0684ad8d500fde579d5b1d46363eef81a7fe3af5
ivanaastankovic <stankovic.ivana1998@gmail.com>
2021-03-15 15:14:59 +0100

    Testiranje

---
+++ test/biblioteka/BibliotekaTest.java
@@ -0,0 +1,23 @@
+package biblioteka;
+
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import biblioteka.interfejs.BibliotekaInterfaceTest;
+
+class BibliotekaTest extends BibliotekaInterfaceTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+		biblioteka = new Biblioteka();
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		biblioteka = null;
+	}
+
+	
+
+}
